3086a7cea94d55519e17d6b9254d324a486e91c6
vdang95 <vdang@gustavus.edu>
2019-01-06 17:58:46 +0700

    Initialize the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
 
-class App extends Component {
+// This is used to download and use some information about the card
+class Color extends Component {
   render() {
     return (
+      <div>
+        <h1> This is a new color {this.props.color} </h1>
+      </div>
+    )
+  } 
+}
+
+class Movie extends Component {
+  render() {
+    const realPosterPath = "https://image.tmdb.org/t/p/w342" + this.props.poster_path;
+    return (
+      <Card id = {this.props.id}>
+        <CardImg
+          top
+          width = {200} 
+          height = {300}
+          src = {realPosterPath} 
+          alt = "Card Image Cap"
+        />
+        <CardBody>
+          <CardTitle> {this.props.title} </CardTitle>
+          <CardSubtitle> {this.props.overview} </CardSubtitle>
+        </CardBody>
+      </Card>
+    )
+  }
+}
+
+class App extends Component {
+
+  async componentDidMount = () => {
+    const response = await fetch();
+    const movieData = await response.json();
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      color: "red",
+      car: "Toyota"
+    }
+  }
+  render() {
+    const listMovies = fakeData.results;
+    return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          listMovies.map((movie) => {
+            return (
+              <div>
+                <Movie 
+                  title = {movie.title}
+                  overview = {movie.overview}
+                  poster_path = {movie.poster_path}
+               />
+              </div>
+            )
+          })
+        }
       </div>
     );
   }
